be27d8bd01725cbc5f908ad3914f3ef7b280c1f1
mariosknl <mariosknl@outlook.com>
2025-05-13 20:13:11 +0300

    Add Sentry integration for error tracking and monitoring; update .gitignore for Sentry config file; create necessary configuration files for server and edge environments.

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://d29088a5d25299e7fe42620306ab0464@o4506953728851968.ingest.us.sentry.io/4509316920508416",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
